fc5a03fc66f771902c7392e0b801208268b3be90
SpikeNew7774 <newadm159@gmail.com>
2025-03-01 23:55:53 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# NonDucky
+*A clone of [Und*ck](https://unduck.link) with some more bangs added.*
+
+DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables all of DuckDuckGo's bangs to work, but much faster.
+
+```
+https://nonducky.spikerko.org?q=%s
+```
+
+## How is it that much faster?
+
+DuckDuckGo does their redirects server side. Their DNS is...not always great. Result is that it often takes ages.
+
+I solved this by doing all of the work client side. Once you've went to https://nonducky.spikerko.org once, the JS is all cache'd and will never need to be downloaded again. Your device does the redirects, not me.
---
+++ src/main.ts
@@ -0,0 +1,85 @@
+import { bangs } from "./bang";
+import "./global.css";
+
+function noSearchDefaultPageRender() {
+  const app = document.querySelector<HTMLDivElement>("#app")!;
+  app.innerHTML = `
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
+      <div class="content-container">
+        <h1>NonDucky</h1>
+        <p style="font-style: italic; color: gray; margin-bottom: .8em;">A clone of <a href="https://unduck.link" target="_blank">Und*ck</a> with some more bangs added.</p>
+        <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a>, and some more</p>
+        <div class="url-container"> 
+          <input 
+            type="text" 
+            class="url-input"
+            value="https://nonducky.spikerko.org?q=%s"
+            readonly 
+          />
+          <button class="copy-button">
+            <img src="/clipboard.svg" alt="Copy" />
+          </button>
+        </div>
+      </div>
+      <footer class="footer">
+        <a href="https://unduck.link" target="_blank">the original</a>
+        •
+        <a href="https://spikerko.org" target="_blank">spikerko.org</a>
+        •
+        <a href="https://github.com/Spikerko/nonducky" target="_blank">github</a>
+      </footer>
+    </div>
+  `;
+
+  const copyButton = app.querySelector<HTMLButtonElement>(".copy-button")!;
+  const copyIcon = copyButton.querySelector("img")!;
+  const urlInput = app.querySelector<HTMLInputElement>(".url-input")!;
+
+  copyButton.addEventListener("click", async () => {
+    await navigator.clipboard.writeText(urlInput.value);
+    copyIcon.src = "/clipboard-check.svg";
+
+    setTimeout(() => {
+      copyIcon.src = "/clipboard.svg";
+    }, 2000);
+  });
+}
+
+const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
+const defaultBang = bangs.find((b) => b.t === LS_DEFAULT_BANG);
+
+function getBangredirectUrl() {
+  const url = new URL(window.location.href);
+  const query = url.searchParams.get("q")?.trim() ?? "";
+  if (!query) {
+    noSearchDefaultPageRender();
+    return null;
+  }
+
+  const match = query.match(/!(\S+)/i);
+
+  const bangCandidate = match?.[1]?.toLowerCase();
+  const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
+
+  // Remove the first bang from the query
+  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
+
+  // Format of the url is:
+  // https://www.google.com/search?q={{{s}}}
+  const searchUrl = selectedBang?.u.replace(
+    "{{{s}}}",
+    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
+    encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
+  );
+  if (!searchUrl) return null;
+
+  return searchUrl;
+}
+
+function doRedirect() {
+  const searchUrl = getBangredirectUrl();
+  if (!searchUrl) return;
+  window.location.replace(searchUrl);
+}
+
+doRedirect();
